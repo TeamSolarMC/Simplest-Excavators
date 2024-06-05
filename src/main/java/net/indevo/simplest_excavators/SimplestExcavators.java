858865b73b175ef1d919566c38cb039559d7730d
DenisSolar <lazarescodr@gmail.com>
2024-06-06 00:39:09 +0300

    Changed: - All excavators stats - All excavators moved to vanilla creative tab - Tools and Utilities
    Fixed:
    - Netherite excavator hadn't fire resistance

--- src/main/java/net/indevo/simplest_excavators/SimplestExcavators.java
+++ src/main/java/net/indevo/simplest_excavators/SimplestExcavators.java
@@ -1,8 +1,8 @@
 package net.indevo.simplest_excavators;
 
 import com.mojang.logging.LogUtils;
-import net.indevo.simplest_excavators.item.ModCreativeModTabs;
 import net.indevo.simplest_excavators.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,8 +24,6 @@ public class SimplestExcavators
     public SimplestExcavators() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModCreativeModTabs.register(modEventBus);
-
         ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
@@ -39,7 +37,14 @@ public class SimplestExcavators
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.WOODEN_EXCAVATOR);
+            event.accept(ModItems.STONE_EXCAVATOR);
+            event.accept(ModItems.IRON_EXCAVATOR);
+            event.accept(ModItems.GOLDEN_EXCAVATOR);
+            event.accept(ModItems.DIAMOND_EXCAVATOR);
+            event.accept(ModItems.NETHERITE_EXCAVATOR);
+        }
     }
 
     @SubscribeEvent
--- src/main/java/net/indevo/simplest_excavators/item/ModItems.java
+++ src/main/java/net/indevo/simplest_excavators/item/ModItems.java
@@ -3,6 +3,7 @@ package net.indevo.simplest_excavators.item;
 import net.indevo.simplest_excavators.SimplestExcavators;
 import net.indevo.simplest_excavators.item.custom.ExcavatorItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,19 +14,17 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, SimplestExcavators.MOD_ID);
 
     public static final RegistryObject<Item> WOODEN_EXCAVATOR = ITEMS.register("wooden_excavator",
-            () -> new ExcavatorItem(ModToolTiers.WOOD, 1.5f, -3.4F, new Item.Properties().durability(88)));
+            () -> new ExcavatorItem(Tiers.WOOD, 2.5f, -3.2F, new Item.Properties().durability(88)));
     public static final RegistryObject<Item> STONE_EXCAVATOR = ITEMS.register("stone_excavator",
-            () -> new ExcavatorItem(ModToolTiers.STONE, 1.5f, -3.4F, new Item.Properties().durability(196)));
+            () -> new ExcavatorItem(Tiers.STONE, 2.5f, -3.2F, new Item.Properties().durability(196)));
     public static final RegistryObject<Item> IRON_EXCAVATOR = ITEMS.register("iron_excavator",
-            () -> new ExcavatorItem(ModToolTiers.IRON, 1.5f, -3.4F, new Item.Properties().durability(375)));
+            () -> new ExcavatorItem(Tiers.IRON, 2.5f, -3.2F, new Item.Properties().durability(375)));
     public static final RegistryObject<Item> GOLDEN_EXCAVATOR = ITEMS.register("golden_excavator",
-            () -> new ExcavatorItem(ModToolTiers.GOLD, 1.5f, -3.4F, new Item.Properties().durability(48)));
+            () -> new ExcavatorItem(Tiers.GOLD, 2.5f, -3.2F, new Item.Properties().durability(48)));
     public static final RegistryObject<Item> DIAMOND_EXCAVATOR = ITEMS.register("diamond_excavator",
-            () -> new ExcavatorItem(ModToolTiers.DIAMOND, 1.5f, -3.4F, new Item.Properties().durability(2341)));
+            () -> new ExcavatorItem(Tiers.DIAMOND, 2.5f, -3.2F, new Item.Properties().durability(2341)));
     public static final RegistryObject<Item> NETHERITE_EXCAVATOR = ITEMS.register("netherite_excavator",
-            () -> new ExcavatorItem(ModToolTiers.NETHERITE, 1.5f, -3.4F, new Item.Properties().durability(3046)));
-
-
+            () -> new ExcavatorItem(Tiers.NETHERITE, 2.5f, -3.2F, new Item.Properties().durability(3046).fireResistant()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
