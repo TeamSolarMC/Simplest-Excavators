b621a963c7a087596cfbf013a77eecd0e729a50c
xeli <sheeteditor2@gmail.com>
2026-01-14 13:19:38 -0500

    datagen changes

--- src/main/java/net/teamsolar/simplest_excavators/item/ModItems.java
+++ src/main/java/net/teamsolar/simplest_excavators/item/ModItems.java
@@ -23,85 +23,85 @@ public class ModItems {
     private static final ChatFormatting TITLE_FORMAT = ChatFormatting.GRAY;
 
     public static final DeferredItem<ExcavatorItem> WOODEN_EXCAVATOR = ITEMS.registerItem(
-            "wooden_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.WOOD,
-                    new Item.Properties(),
-                    2.5f,
-                    -3.2f,
-                    177
-            )
+        "wooden_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.WOOD,
+            new Item.Properties(),
+            2.5f,
+            -3.2f,
+            177
+        )
     );
     public static final DeferredItem<ExcavatorItem> STONE_EXCAVATOR = ITEMS.registerItem(
-            "stone_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.STONE,
-                    new Item.Properties(),
-                    2.5f,
-                    -3.2f,
-                    393
-            )
+        "stone_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.STONE,
+            new Item.Properties(),
+            2.5f,
+            -3.2f,
+            393
+        )
     );
     public static final DeferredItem<ExcavatorItem> IRON_EXCAVATOR = ITEMS.registerItem(
-            "iron_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.IRON,
-                    new Item.Properties(),
-                    2.5f,
-                    -3.2f,
-                    750
-            )
+        "iron_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.IRON,
+            new Item.Properties(),
+            2.5f,
+            -3.2f,
+            750
+        )
     );
     public static final DeferredItem<ExcavatorItem> GOLDEN_EXCAVATOR = ITEMS.registerItem(
-            "golden_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.GOLD,
-                    new Item.Properties(),
-                    2.5f,
-                    -3.2f,
-                    96
-            )
+        "golden_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.GOLD,
+            new Item.Properties(),
+            2.5f,
+            -3.2f,
+            96
+        )
     );
     public static final DeferredItem<ExcavatorItem> DIAMOND_EXCAVATOR = ITEMS.registerItem(
-            "diamond_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.DIAMOND,
-                    new Item.Properties(),
-                    2.5f,
-                    -3.2f,
-                    4683
-            )
+        "diamond_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.DIAMOND,
+            new Item.Properties(),
+            2.5f,
+            -3.2f,
+            4683
+        )
     );
     public static final DeferredItem<ExcavatorItem> NETHERITE_EXCAVATOR = ITEMS.registerItem(
-            "netherite_excavator", ExcavatorItem::new,
-            () -> ExcavatorItem.excavatorProperties(
-                    ToolMaterial.NETHERITE,
-                    new Item.Properties().fireResistant(),
-                    2.5f,
-                    -3.2f,
-                    6093
-            )
+        "netherite_excavator", ExcavatorItem::new,
+        () -> ExcavatorItem.excavatorProperties(
+            ToolMaterial.NETHERITE,
+            new Item.Properties().fireResistant(),
+            2.5f,
+            -3.2f,
+            6093
+        )
     );
 
     public static final DeferredItem<SmithingTemplateItem> EXCAVATOR_SMITHING_TEMPLATE = ITEMS.registerItem("excavator_smithing_template",
-            (properties) -> new SmithingTemplateItem(
-                    Component.translatable("item.simplest_excavators.excavator_smithing_template.applies_to").withStyle(DESCRIPTION_FORMAT), // DESCRIPTION_FORMAT
-                    Component.translatable("item.simplest_excavators.excavator_smithing_template.ingredients").withStyle(DESCRIPTION_FORMAT), // DESCRIPTION_FORMAT
-                    // Component.translatable("item.simplest_hammers.hammer_smithing_template.upgrade_description").withStyle(ChatFormatting.GRAY),
-                    // Upgrade descriptions were removed in 1.21.10
-                    Component.translatable("item.simplest_excavators.excavator_smithing_template.base_slot_description"), // No formatting
-                    Component.translatable("item.simplest_excavators.excavator_smithing_template.additions_slot_description"), // No formatting
-                    // Base slot empty icons
-                    List.of(
-                            ResourceLocation.withDefaultNamespace("item/empty_slot_shovel"),
-                            ResourceLocation.fromNamespaceAndPath(SimplestExcavators.MODID, "item/empty_slot_excavator")
-                    ),
-                    // Additional slot empty icons
-                    List.of(
-                            ResourceLocation.fromNamespaceAndPath(SimplestExcavators.MODID, "item/empty_slot_block")
-                    ),
-                    properties
-            )
+        (properties) -> new SmithingTemplateItem(
+            Component.translatable("item.simplest_excavators.excavator_smithing_template.applies_to").withStyle(DESCRIPTION_FORMAT), // DESCRIPTION_FORMAT
+            Component.translatable("item.simplest_excavators.excavator_smithing_template.ingredients").withStyle(DESCRIPTION_FORMAT), // DESCRIPTION_FORMAT
+            // Component.translatable("item.simplest_hammers.hammer_smithing_template.upgrade_description").withStyle(ChatFormatting.GRAY),
+            // Upgrade descriptions were removed in 1.21.10
+            Component.translatable("item.simplest_excavators.excavator_smithing_template.base_slot_description"), // No formatting
+            Component.translatable("item.simplest_excavators.excavator_smithing_template.additions_slot_description"), // No formatting
+            // Base slot empty icons
+            List.of(
+                ResourceLocation.withDefaultNamespace("item/empty_slot_shovel"),
+                ResourceLocation.fromNamespaceAndPath(SimplestExcavators.MODID, "item/empty_slot_excavator")
+            ),
+            // Additional slot empty icons
+            List.of(
+                ResourceLocation.fromNamespaceAndPath(SimplestExcavators.MODID, "item/empty_slot_block")
+            ),
+            properties
+        )
     );
 
     public static void register(IEventBus eventBus) {
